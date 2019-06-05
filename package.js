e5ccffa681927c769465d24f3168edc636448721
Tuan Dinh <tuandtb@dgroup.co>
2019-06-05 12:09:43 +0700

    Upgrade version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: "https://github.com/tuandtb/legacy.git",
   name: "baotuan:active-route",
   summary: "Active route helpers",
-  version: "2.3.3-rc.4"
+  version: "2.3.3-rc.5"
 });
 
 Package.onUse(function(api) {
