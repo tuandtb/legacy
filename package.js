8b352bf2183127e069de564e742346d883bdf8ae
Tuan Dinh <tuandtb@dgroup.co>
2019-06-05 12:04:55 +0700

    Remove testing

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: "https://github.com/tuandtb/legacy.git",
   name: "baotuan:active-route",
   summary: "Active route helpers",
-  version: "2.3.3-rc.3"
+  version: "2.3.3-rc.4"
 });
 
 Package.onUse(function(api) {
@@ -26,29 +26,4 @@ Package.onUse(function(api) {
   api.addFiles("lib/activeroute.coffee");
 
   api.addFiles("client/helpers.coffee", "client");
-});
-
-Package.onTest(function(api) {
-  api.versionsFrom(["1.0", "1.2"]);
-
-  api.use([
-    "check",
-    "coffeescript",
-    "reactive-dict",
-    "templating",
-    "underscore"
-  ]);
-
-  api.use([
-    "practicalmeteor:mocha@2.1.0_5",
-    "practicalmeteor:chai@2.1.0_1",
-    "zimme:active-route"
-  ]);
-
-  api.addFiles(
-    ["tests/client/activeroute.coffee", "tests/client/helpers.coffee"],
-    "client"
-  );
-
-  api.addFiles("tests/server/activeroute.coffee", "server");
 });
